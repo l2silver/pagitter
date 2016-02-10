596d08600399ae18c48cb748726d3b5822f8a038
leigh <l2silver@ryerson.ca>
2016-02-10 00:38:41 -0500

    pagitter-store finished writing

--- __test__/pagitter-store_spec.js
+++ __test__/pagitter-store_spec.js
@@ -1,13 +1,17 @@
 import {expect} from 'chai';
 import {promisify} from 'bluebird';
-import {Map, fromJS} from 'immutable';
+import {Map, fromJS, List} from 'immutable';
 import fs from 'fs';
 import rimraf from 'rimraf'
+import mkdirp from 'mkdirp';
 import generatePromise
 	,{
-		addNewStores
+		  checkNewStores
+		, addNewStores
 		, newStoresCreate
 		, addContent
+		, checkEndStores
+		, setEndStoreNames
 } from './../lib/pagitter-store';
 const fsExists = promisify(fs.exists);
 const readFile = promisify(fs.readFile);
@@ -15,15 +19,20 @@ const readFile = promisify(fs.readFile);
 
 
 describe('pagitter-store', ()=>{
-
-	before(()=>{
-		rimraf.sync('example');
+	it('checkNewStores', ()=>{
+		const state = fromJS({
+			globalVariables: {
+				pagitterStoresCreate: 'signup'
+			}
+		});
+		;
+		expect(checkNewStores(state)).to.eventually.equal(
+			fromJS({
+			  globalVariables: {}
+			, pagitterStores: {signup: ''}
+			})
+		);	
 	});
-
-	after(()=>{
-		rimraf.sync('example');
-	});
-
 	it('addNewStores', ()=>{
 		const state = fromJS({
 			globalVariables: {
@@ -43,6 +52,11 @@ describe('pagitter-store', ()=>{
 			Map({ signup: ''})
 		);	
 	});
+	it('newStoresCreate array', ()=>{
+		expect(newStoresCreate('["signup", "logout"]')).to.equal(
+			Map({ signup: '', logout: ''})
+		);	
+	});	
 	it('addContent', ()=>{
 		const pagitterStores = Map({signup: ''});
 		const state = Map({
@@ -58,5 +72,106 @@ describe('pagitter-store', ()=>{
 				code: '\ncode',
 				content: '\ncontent'})
 		);	
+	});
+	describe('checkEndStores', ()=>{
+		before(()=>{
+			mkdirp.sync('.pagitterStores');
+		});
+		after(()=>{
+			rimraf.sync('.pagitterStores');
+		});
+
+		it('returns state', ()=>{
+			const pagitterStores = Map({signup: '\ncode\ncontent'});
+			const globalVariables = Map({
+				pagitterStoresEnd: 'signup'
+			})
+			const state = Map({
+				pagitterStores,
+				globalVariables
+			})
+			expect(checkEndStores(state)).to.eventually.equal(
+				fromJS({
+					pagitterStores: {
+					},
+					globalVariables: {}
+				})
+			);	
+		});
+		it('writes store', (done)=>{
+			const pagitterStores = Map({signup: '\ncode\ncontent'});
+			const globalVariables = Map({
+				pagitterStoresEnd: 'signup'
+			})
+			const state = Map({
+				pagitterStores,
+				globalVariables
+			})
+			checkEndStores(state)
+			.then(()=>{
+				return readFile('.pagitterStores/signup.js','utf8')
+			})
+			.then((content)=>{
+				expect(content).to.equal('\ncode\ncontent');
+				done()
+			})
+		});	
+	})
+	
+	it('setEndStoreNames', ()=>{
+		const pagitterStores = Map({signup: '\ncode\ncontent'});
+		const globalVariables = Map({
+			pagitterStoresEnd: 'signup'
+		});
+		const state = Map({
+			pagitterStores,
+			globalVariables
+		});
+		expect(setEndStoreNames(state)).to.equal(
+			Map({
+				pagitterStores: Map({
+					signup: '\ncode\ncontent'
+				}),
+				globalVariables,
+				pagitterStoresEnd: List(['signup'])
+			})
+		);	
+	});
+	describe('generatePromise', ()=>{
+		before(()=>{
+			mkdirp.sync('.pagitterStores');
+		});
+		after(()=>{
+			rimraf.sync('.pagitterStores');
+		});
+		const globalVariables = Map({
+			pagitterStoresCreate: 'signup',
+			pagitterStoresEnd: 'signup'
+		});
+		const state = Map({
+			globalVariables,
+			code: '\ncode',
+			content: '\ncontent'
+		})
+		it('returns state', ()=>{
+			expect(generatePromise(state)).to.eventually.equal(
+				fromJS({
+					pagitterStores: {},
+					globalVariables: {},
+					code: '\ncode',
+					content: '\ncontent'
+				})
+			);
+		});
+		it('creates file', (done)=>{
+			generatePromise(state)
+			.then(()=>{
+				return readFile('.pagitterStores/signup.js','utf8')
+			})
+			.then((content)=>{
+				expect(content).to.equal('\ncode\ncontent');
+				done()
+			})
+		});
 	});
 });
